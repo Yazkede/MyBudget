a3c9d5f2ef41b243be3d06ff858e6dbaaee29ddd
Alexandr Rybin <gelikopterfl@gmail.com>
2021-10-28 19:09:54 +0300

    off

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 const BUDGET = document.querySelector('.budget');
 const FOOD = document.querySelector('.food');
 const PHARMACY_S = document.querySelector('.pharm1');
-const PHARMACY_X = document.querySelector('.pharm2');
 const COMMUNAL_S = document.querySelector('.comm1');
-const COMMUNAL_X = document.querySelector('.comm2');
 const CLOTHES = document.querySelector('.clothes');
 const SCHOOL = document.querySelector('.school');
 const HOSPITAL = document.querySelector('.hospital');
-const CAT = document.querySelector('.cat');
+const PET = document.querySelector('.pet');
 const UNEXPECTED = document.querySelector('.unexpected');
 const BUTTON_SAVE = document.querySelector('.field__btn-save');
 const BUTTON_SHOW = document.querySelector('.field__btn-show');
@@ -17,13 +15,11 @@ const BUTTON_CLEAR = document.querySelector('.field__btn-clear');
 let budgetData = document.querySelector('.field__budget-data');
 let foodData = document.querySelector('.field__food-data');
 let pharmSdata = document.querySelector('.field__pharmacy-s-data');
-let pharmXdata = document.querySelector('.field__pharmacy-x-data');
 let communalSdata = document.querySelector('.field__communal-s-data');
-let communalXdata = document.querySelector('.field__communal-x-data');
 let clothesData = document.querySelector('.field__clothes-data');
 let schoolData = document.querySelector('.field__school-data');
 let hospitalData = document.querySelector('.field__hospital-data');
-let catData = document.querySelector('.field__cat-data');
+let petData = document.querySelector('.field__pet-data');
 let unexpectedData = document.querySelector('.field__unexpected-data');
 let economy = document.querySelector('.field__economy-data');
 
@@ -37,13 +33,11 @@ function onSaveDataClick() {
     countBudget();
     countFood();
     countPharmacyS();
-    countPharmacyX();
     countCommunalS();
-    countCommunalX();
     countClothes();
     countSchool();
     countHospital();
-    countCat();
+    countPet();
     countUnexpected();
     countEconomy();
 }
@@ -53,13 +47,11 @@ function onGetDataClick() {
     budgetData.textContent = localStorage.getItem('budget');
     foodData.textContent = localStorage.getItem('food');
     pharmSdata.textContent = localStorage.getItem('pharm1');
-    pharmXdata.textContent = localStorage.getItem('pharm2');
     communalSdata.textContent = localStorage.getItem('comm1');
-    communalXdata.textContent = localStorage.getItem('comm2');
     clothesData.textContent = localStorage.getItem('clothes');
     schoolData.textContent = localStorage.getItem('school');
     hospitalData.textContent = localStorage.getItem('hospital');
-    catData.textContent = localStorage.getItem('cat');
+    petData.textContent = localStorage.getItem('pet');
     unexpectedData.textContent = localStorage.getItem('unexpected');
     economy.textContent = localStorage.getItem('economy');
 }
@@ -84,147 +76,192 @@ function onClearDataClick() {
 }
 
 function countBudget() {
-    if (localStorage.getItem('budget') !== null) {
+    if (localStorage.getItem('budget') !== null && BUDGET.value !== '') {
         let newBudget = parseFloat(BUDGET.value) + parseFloat(localStorage.getItem('budget'));
         localStorage.setItem('budget', newBudget);
         budgetData.textContent = localStorage.getItem('budget');
-    } else {
-        localStorage.setItem('budget', parseFloat(BUDGET.value));
-        budgetData.textContent = parseFloat(BUDGET.value);
+        BUDGET.value = '';
+    } else if (localStorage.getItem('budget') !== null && BUDGET.value === '') {
+        let newBudget = localStorage.getItem('budget');
+        localStorage.setItem('budget', newBudget);
+        budgetData.textContent = localStorage.getItem('budget');
+    } else if (localStorage.getItem('budget') === null && BUDGET.value === '') {
+        localStorage.setItem('budget', 0);
+        budgetData.textContent = localStorage.getItem('budget');
+    } else if (localStorage.getItem('budget') === null && BUDGET.value !== '') {
+        localStorage.setItem('budget', BUDGET.value);
+        budgetData.textContent = localStorage.getItem('budget');
+        BUDGET.value = '';
     }
-    let budget = localStorage.getItem('budget');
-    return budget;
 }
 
 function countFood() {
-    if (localStorage.getItem('food') !== null) {
+    if (localStorage.getItem('food') !== null && FOOD.value !== '') {
         let newFood = parseFloat(FOOD.value) + parseFloat(localStorage.getItem('food'));
         localStorage.setItem('food', newFood);
         foodData.textContent = localStorage.getItem('food');
-    } else {
-        localStorage.setItem('food', parseFloat(FOOD.value));
-        foodData.textContent = parseFloat(FOOD.value);
+        FOOD.value = '';
+    } else if (localStorage.getItem('food') !== null && FOOD.value === '') {
+        let newFood = localStorage.getItem('food');
+        localStorage.setItem('food', newFood);
+        foodData.textContent = localStorage.getItem('food');
+    } else if (localStorage.getItem('food') === null && FOOD.value === '') {
+        localStorage.setItem('food', 0);
+        foodData.textContent = localStorage.getItem('food');
+    } else if (localStorage.getItem('food') === null && FOOD.value !== '') {
+        localStorage.setItem('food', FOOD.value);
+        foodData.textContent = localStorage.getItem('food');
+        FOOD.value = '';
     }
-    let food = localStorage.getItem('food');
-    return food;
 }
 
-console.log(FOOD.value)
-
 function countPharmacyS() {
-    if (localStorage.getItem('pharm1') !== null) {
+    if (localStorage.getItem('pharm1') !== null && PHARMACY_S.value !== '') {
         let newPharm = parseFloat(PHARMACY_S.value) + parseFloat(localStorage.getItem('pharm1'));
         localStorage.setItem('pharm1', newPharm);
         pharmSdata.textContent = localStorage.getItem('pharm1');
-    } else {
-        localStorage.setItem('pharm1', parseFloat(PHARMACY_S.value));
-        pharmSdata.textContent = parseFloat(PHARMACY_S.value);
+        PHARMACY_S.value = '';
+    } else if (localStorage.getItem('pharm1') !== null && PHARMACY_S.value === '') {
+        let newPharm = localStorage.getItem('pharm1');
+        localStorage.setItem('pharm1', newPharm);
+        pharmSdata.textContent = localStorage.getItem('pharm1');
+    } else if (localStorage.getItem('pharm1') === null && PHARMACY_S.value === '') {
+        localStorage.setItem('pharm1', 0);
+        pharmSdata.textContent = localStorage.getItem('pharm1');
+    } else if (localStorage.getItem('pharm1') === null && PHARMACY_S.value !== '') {
+        localStorage.setItem('pharm1', PHARMACY_S.value);
+        pharmSdata.textContent = localStorage.getItem('pharm1');
+        PHARMACY_S.value = '';
     }
-    let pharm1 = localStorage.getItem('pharm1');
-    return pharm1;
 }
-function countPharmacyX() {
-    if (localStorage.getItem('pharm2') !== null) {
-        let newPharm = parseFloat(PHARMACY_X.value) + parseFloat(localStorage.getItem('pharm2'));
-        localStorage.setItem('pharm2', newPharm);
-        pharmXdata.textContent = localStorage.getItem('pharm2');
-    } else {
-        localStorage.setItem('pharm2', parseFloat(PHARMACY_X.value));
-        pharmXdata.textContent = parseFloat(PHARMACY_X.value);
-    }
-    let pharm2 = localStorage.getItem('pharm2');
-    return pharm2;
-}
+
 function countCommunalS() {
-    if (localStorage.getItem('comm1') !== null) {
+    if (localStorage.getItem('comm1') !== null && COMMUNAL_S.value !== '') {
         let newComm = parseFloat(COMMUNAL_S.value) + parseFloat(localStorage.getItem('comm1'));
         localStorage.setItem('comm1', newComm);
         communalSdata.textContent = localStorage.getItem('comm1');
-    } else {
-        localStorage.setItem('comm1', parseFloat(COMMUNAL_S.value));
-        communalSdata.textContent = parseFloat(COMMUNAL_S.value);
+        COMMUNAL_S.value = '';
+    } else if (localStorage.getItem('comm1') !== null && COMMUNAL_S.value === '') {
+        let newComm = localStorage.getItem('comm1');
+        localStorage.setItem('comm1', newComm);
+        communalSdata.textContent = localStorage.getItem('comm1');
+    } else if (localStorage.getItem('comm1') === null && COMMUNAL_S.value === '') {
+        localStorage.setItem('comm1', 0);
+        communalSdata.textContent = localStorage.getItem('comm1');
+    } else if (localStorage.getItem('comm1') === null && COMMUNAL_S.value !== '') {
+        localStorage.setItem('comm1', COMMUNAL_S.value);
+        communalSdata.textContent = localStorage.getItem('comm1');
+        COMMUNAL_S.value = '';
     }
-    let communal1 = localStorage.getItem('communal1');
-    return communal1;
 }
-function countCommunalX() {
-    if (localStorage.getItem('comm2') !== null) {
-        let newComm = parseFloat(COMMUNAL_X.value) + parseFloat(localStorage.getItem('comm2'));
-        localStorage.setItem('comm2', newComm);
-        communalXdata.textContent = localStorage.getItem('comm2');
-    } else {
-        localStorage.setItem('comm2', parseFloat(COMMUNAL_X.value));
-        communalXdata.textContent = parseFloat(COMMUNAL_X.value);
-    }
-    let communal2 = localStorage.getItem('communal2');
-    return communal2;
-}
+
 function countClothes() {
-    if (localStorage.getItem('clothes') !== null) {
-        let newClothe = parseFloat(CLOTHES.value) + parseFloat(localStorage.getItem('clothes'));
-        localStorage.setItem('clothes', newClothe);
+    if (localStorage.getItem('clothes') !== null && CLOTHES.value !== '') {
+        let newClothes = parseFloat(CLOTHES.value) + parseFloat(localStorage.getItem('clothes'));
+        localStorage.setItem('clothes', newClothes);
         clothesData.textContent = localStorage.getItem('clothes');
-    } else {
-        localStorage.setItem('clothes', parseFloat(CLOTHES.value));
-        clothesData.textContent = parseFloat(CLOTHES.value);
+        CLOTHES.value = '';
+    } else if (localStorage.getItem('clothes') !== null && CLOTHES.value === '') {
+        let newClothes = localStorage.getItem('clothes');
+        localStorage.setItem('clothes', newClothes);
+        clothesData.textContent = localStorage.getItem('clothes');
+    } else if (localStorage.getItem('clothes') === null && CLOTHES.value === '') {
+        localStorage.setItem('clothes', 0);
+        clothesData.textContent = localStorage.getItem('clothes');
+    } else if (localStorage.getItem('clothes') === null && CLOTHES.value !== '') {
+        localStorage.setItem('clothes', CLOTHES.value);
+        clothesData.textContent = localStorage.getItem('clothes');
+        CLOTHES.value = '';
     }
-    let clothes = localStorage.getItem('clothes');
-    return clothes;
 }
 function countSchool() {
-    if (localStorage.getItem('school') !== null) {
+    if (localStorage.getItem('school') !== null && SCHOOL.value !== '') {
         let newSchool = parseFloat(SCHOOL.value) + parseFloat(localStorage.getItem('school'));
         localStorage.setItem('school', newSchool);
         schoolData.textContent = localStorage.getItem('school');
-    } else {
-        localStorage.setItem('school', parseFloat(SCHOOL.value));
-        schoolData.textContent = parseFloat(SCHOOL.value);
+        SCHOOL.value = '';
+    } else if (localStorage.getItem('school') !== null && SCHOOL.value === '') {
+        let newSchool = localStorage.getItem('school');
+        localStorage.setItem('school', newSchool);
+        schoolData.textContent = localStorage.getItem('school');
+    } else if (localStorage.getItem('school') === null && SCHOOL.value === '') {
+        localStorage.setItem('school', 0);
+        schoolData.textContent = localStorage.getItem('school');
+    } else if (localStorage.getItem('school') === null && SCHOOL.value !== '') {
+        localStorage.setItem('school', SCHOOL.value);
+        schoolData.textContent = localStorage.getItem('school');
+        SCHOOL.value = '';
     }
-    let school = localStorage.getItem('school');
-    return school;
 }
 function countHospital() {
-    if (localStorage.getItem('hospital') !== null) {
+    if (localStorage.getItem('hospital') !== null && HOSPITAL.value !== '') {
         let newHospital = parseFloat(HOSPITAL.value) + parseFloat(localStorage.getItem('hospital'));
         localStorage.setItem('hospital', newHospital);
         hospitalData.textContent = localStorage.getItem('hospital');
-    } else {
-        localStorage.setItem('hospital', parseFloat(HOSPITAL.value));
-        hospitalData.textContent = parseFloat(HOSPITAL.value);
+        HOSPITAL.value = '';
+    } else if (localStorage.getItem('hospital') !== null && HOSPITAL.value === '') {
+        let newHospital = localStorage.getItem('hospital');
+        localStorage.setItem('hospital', newHospital);
+        hospitalData.textContent = localStorage.getItem('hospital');
+    } else if (localStorage.getItem('hospital') === null && HOSPITAL.value === '') {
+        localStorage.setItem('hospital', 0);
+        hospitalData.textContent = localStorage.getItem('hospital');
+    } else if (localStorage.getItem('hospital') === null && HOSPITAL.value !== '') {
+        localStorage.setItem('hospital', HOSPITAL.value);
+        hospitalData.textContent = localStorage.getItem('hospital');
+        HOSPITAL.value = '';
     }
-    let hospital = localStorage.getItem('hospital');
-    return hospital;
 }
-function countCat() {
-    if (localStorage.getItem('cat') !== null) {
-        let newCat = parseFloat(CAT.value) + parseFloat(localStorage.getItem('cat'));
-        localStorage.setItem('cat', newCat);
-        catData.textContent = localStorage.getItem('cat');
-    } else {
-        localStorage.setItem('cat', parseFloat(CAT.value));
-        catData.textContent = parseFloat(CAT.value);
+function countPet() {
+    if (localStorage.getItem('pet') !== null && PET.value !== '') {
+        let newPet = parseFloat(PET.value) + parseFloat(localStorage.getItem('pet'));
+        localStorage.setItem('pet', newPet);
+        petData.textContent = localStorage.getItem('pet');
+        PET.value = '';
+    } else if (localStorage.getItem('pet') !== null && PET.value === '') {
+        let newPet = localStorage.getItem('pet');
+        localStorage.setItem('pet', newPet);
+        petData.textContent = localStorage.getItem('pet');
+    } else if (localStorage.getItem('pet') === null && PET.value === '') {
+        localStorage.setItem('pet', 0);
+        petData.textContent = localStorage.getItem('pet');
+    } else if (localStorage.getItem('pet') === null && PET.value !== '') {
+        localStorage.setItem('pet', PET.value);
+        petData.textContent = localStorage.getItem('pet');
+        PET.value = '';
     }
-    let cat = localStorage.getItem('cat');
-    return cat;
 }
 function countUnexpected() {
-    if (localStorage.getItem('unexpected') !== null) {
+    if (localStorage.getItem('unexpected') !== null && UNEXPECTED.value !== '') {
         let newUnexpected = parseFloat(UNEXPECTED.value) + parseFloat(localStorage.getItem('unexpected'));
         localStorage.setItem('unexpected', newUnexpected);
         unexpectedData.textContent = localStorage.getItem('unexpected');
-    } else {
-        localStorage.setItem('unexpected', parseFloat(UNEXPECTED.value));
-        unexpectedData.textContent = parseFloat(UNEXPECTED.value);
+        UNEXPECTED.value = '';
+    } else if (localStorage.getItem('unexpected') !== null && UNEXPECTED.value === '') {
+        let newUnexpected = localStorage.getItem('unexpected');
+        localStorage.setItem('unexpected', newUnexpected);
+        unexpectedData.textContent = localStorage.getItem('unexpected');
+    } else if (localStorage.getItem('unexpected') === null && UNEXPECTED.value === '') {
+        localStorage.setItem('unexpected', 0);
+        unexpectedData.textContent = localStorage.getItem('unexpected');
+    } else if (localStorage.getItem('unexpected') === null && UNEXPECTED.value !== '') {
+        localStorage.setItem('unexpected', UNEXPECTED.value);
+        unexpectedData.textContent = localStorage.getItem('unexpected');
+        UNEXPECTED.value = '';
     }
-    let unexpected = localStorage.getItem('unexpected');
-    return unexpected;
 }
 function countEconomy() {
     let result;
-    result = BUDGET.value - parseFloat(food) - parseFloat(pharm1) - parseFloat(pharm2)
-        - parseFloat(communal1) - parseFloat(communal2) - parseFloat(clothes) - parseFloat(school)
-        - parseFloat(hospital) - parseFloat(cat) - parseFloat(unexpected);
+    result = (parseFloat(localStorage.getItem('budget'))
+        - parseFloat(localStorage.getItem('food'))
+        - parseFloat(localStorage.getItem('pharm1'))
+        - parseFloat(localStorage.getItem('comm1'))
+        - parseFloat(localStorage.getItem('clothes'))
+        - parseFloat(localStorage.getItem('school'))
+        - parseFloat(localStorage.getItem('hospital'))
+        - parseFloat(localStorage.getItem('pet'))
+        - parseFloat(localStorage.getItem('unexpected')));
 
     localStorage.setItem('economy', result);
-    budgetData.textContent = localStorage.getItem('economy');
+    economy.textContent = localStorage.getItem('economy');
 }
